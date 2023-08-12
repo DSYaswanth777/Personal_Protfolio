9110df0a221ac03839b5686e52e3784e704a0bc1
DSYASWANTH777 <dsyaswanth777@gmail.com>
2023-08-12 15:26:17 +0530

    Removal of unused files

--- src/components/Skils_Section.jsx
+++ src/components/Skils_Section.jsx
@@ -18,7 +18,7 @@ const Skils_Section = forwardRef((props, ref) => {
             <div className="skills-section pt-10">
               <p>HTML</p> <p>CSS</p> <p>Javascript</p> <p>Bootstrap</p>{" "}
               <p>Sass</p> <p>tailwind CSS</p> <p>Bootstrap</p> <p>React.Js</p>{" "}
-              <p>Next.Js</p>
+              <p>Next.Js</p><p>Github</p>
               <p>Python</p> <p>C++</p>
             </div>
           </div>
