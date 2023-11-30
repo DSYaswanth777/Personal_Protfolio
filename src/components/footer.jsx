216594671b7d0f7036323c70410c709a8b302b26
DSYASWANTH777 <dsyaswanth777@gmail.com>
2023-11-30 12:16:13 +0530

    updated(Added SEO accesbiity and tag updation)

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,42 +9,42 @@ function footer() {
     <footer className="bg-customBlack">
       <div className=" container md:mx-auto flex justify-between items-center h-full py-16 gap-8  flex-col px-6">
         <Reveal2>
-
-        <p className="text-white font-extra-bold">&copy;2023.Yaswanth Dasari</p>
+          <p className="text-white font-extra-bold">
+            &copy;2023.Yaswanth Dasari
+          </p>
         </Reveal2>
         <Reveal2>
-<nav aria-label="Footer Social Links">
-
-        <div className="flex flex-row gap-3">
-          <a
-            href="https://github.com/DSYaswanth777"
-            target="_blank"
-            className="bg-white rounded-full p-3 border-4 shadow-xl relative group zoom-effect "
-          >
-            <div className="flex items-center">
-              <FaGithub size={20} />
+          <nav aria-label="Footer Social Links">
+            <div className="flex flex-row gap-3">
+              <a
+                href="https://github.com/DSYaswanth777"
+                target="_blank"
+                className="bg-white rounded-full p-3 border-4 shadow-xl relative group zoom-effect "
+              >
+                <div className="flex items-center">
+                  <FaGithub size={20} />
+                </div>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/dasari-srinivasa-yaswanth-7a752619b/"
+                target="_blank"
+                className="bg-white rounded-full p-3  border-4 shadow-xl relative group zoom-effect "
+              >
+                <div className="flex items-center">
+                  <FaLinkedinIn size={20} />
+                </div>
+              </a>
+              <a
+                href="mailto:dsyaswanth777@gmail.com"
+                target="_blank"
+                className="bg-white rounded-full p-3  border-4 shadow-xl relative group zoom-effect "
+              >
+                <div className="flex items-center">
+                  <HiOutlineMailOpen size={20} />
+                </div>
+              </a>
             </div>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/dasari-srinivasa-yaswanth-7a752619b/"
-            target="_blank"
-            className="bg-white rounded-full p-3  border-4 shadow-xl relative group zoom-effect "
-          >
-            <div className="flex items-center">
-              <FaLinkedinIn size={20} />
-            </div>
-          </a>
-          <a
-            href="mailto:dsyaswanth777@gmail.com"
-            target="_blank"
-            className="bg-white rounded-full p-3  border-4 shadow-xl relative group zoom-effect "
-          >
-            <div className="flex items-center">
-              <HiOutlineMailOpen size={20} />
-            </div>
-          </a>
-        </div>
-</nav>
+          </nav>
         </Reveal2>
       </div>
     </footer>
